933627806d9ab2e8a19d9c708d0dc9f5261cb511
Abril Gavilan <abrilcgavilan@gmail.com>
2026-04-15 14:54:26 -0300

    Creación del hero, vista atrapante.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import styles from "./Home.module.css";
 
 import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <Navbar />
-
+      <Hero />
       <Footer />
     </div>
   );
