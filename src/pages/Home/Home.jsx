7b80aca402b0bf92a3ae47697334dc67f8d20d64
Abril Gavilan <abrilcgavilan@gmail.com>
2026-04-16 23:47:45 -0300

    Creación de la sección "Proyectos en Marcha" y suma de respectivas imagenes. Archivos modificados: Projects.jsx, Projects.module.css y Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import PersonalInfo from "../../components/PersonalInfo/PersonalInfo";
+import Projects from "../../components/Projects/Projects";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <PersonalInfo />
+      <Projects />
       <Footer />
     </div>
   );
